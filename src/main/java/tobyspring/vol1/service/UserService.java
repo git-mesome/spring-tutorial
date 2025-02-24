487f27a502c80252be13372e53ded8a5309fdf56
Minseo Kim <kms12782@gmail.com>
2025-02-24 13:07:10 +0900

    JavaMail 인터페이스 구현

--- src/main/java/tobyspring/vol1/service/UserService.java
+++ src/main/java/tobyspring/vol1/service/UserService.java
@@ -1,5 +1,11 @@
 package tobyspring.vol1.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -8,8 +14,9 @@ import tobyspring.vol1.domain.Level;
 import tobyspring.vol1.domain.User;
 
 import java.util.List;
+import java.util.Properties;
 
-public class UserService  {
+public class UserService {
 
   private UserDao userDao;
   private UserLevelUpgradePolicy upgradePolicy;
@@ -40,14 +47,35 @@ public class UserService  {
         if (upgradePolicy.canUpgradeLevel(user)) {
           upgradePolicy.upgradeLevels(user);
           userDao.update(user);
+          sendUpgradeEmail(user);
         }
 
       }
       this.transactionManager.commit(status);
 
-    }catch (Exception e){
+    } catch (Exception e) {
       this.transactionManager.rollback(status);
       throw e;
+    }
+
+  }
+
+  // 한글 인코딩 생략
+  private void sendUpgradeEmail(User user) {
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "mail.ksug.org");
+    Session session = Session.getDefaultInstance(props, null);
+
+    MimeMessage message = new MimeMessage(session);
+    try {
+      message.setFrom(new InternetAddress("useradmin@ksug.org"));
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+      message.setSubject("Upgrade 안내");
+      message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+    } catch (AddressException e) {
+      throw new RuntimeException(e);
+    } catch (MessagingException e) {
+      throw new RuntimeException(e);
     }
 
   }
